2b6c58ceb13cd656d34ae30c568d507dda10c71f
Alent <wangyingli1993@163.com>
2017-03-25 14:19:23 +0800

    Initial commit

---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Alent
+  Date: 2017/3/25
+  Time: 14:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>测试</title>
+</head>
+<body>
+这是测试页面
+</body>
+</html>
---
+++ src/table.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE blog;
+USE blog;
+
+# MySQL5.0版本以上，varchar(50)，指的是可以存放50字符，无论存放的是数字、字母还是UTF8汉字，都只能存放50个
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  id       INT(11) PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
+  username VARCHAR(20) NOT NULL COMMENT '用户名',
+  password VARCHAR(32)         DEFAULT NULL COMMENT '密码',
+  nickname VARCHAR(20)         DEFAULT NULL COMMENT '昵称',
+  email    VARCHAR(32)         DEFAULT NULL COMMENT '邮箱',
+  sign     VARCHAR(80)         DEFAULT NULL COMMENT '签名',
+  avatar   VARCHAR(80)         DEFAULT NULL COMMENT '头像'
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8 COMMENT '用户';
+# AUTO_INCREMENT=1表示主键自增从1开始
+
+INSERT INTO user (username, password, nickname, email, sign, avatar)
+  VALUE ('Alent', '123', 'Gain', '123@163.com', '一切皆有可能', 'http:baidu.com');
+
+DROP TABLE IF EXISTS blog;
+CREATE TABLE blog (
+  id          INT(11) PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
+  user_id     INT(11)      NOT NULL COMMENT '用户id',
+  title       VARCHAR(100) NOT NULL COMMENT '标题',
+  content     TEXT COMMENT '正文',
+  create_time DATETIME            DEFAULT NULL COMMENT '创建时间',
+  publish_time DATETIME            DEFAULT NULL COMMENT '发布时间',
+  state TINYINT DEFAULT 0 COMMENT '状态：发布1，未发布0',
+  read_num INT(11) DEFAULT 0 COMMENT '阅读数',
+  FOREIGN KEY (id) REFERENCES user (id)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8 COMMENT '博客';
+
+
+DROP TABLE IF EXISTS 'comment';
+CREATE TABLE 'comment' (
+  id           INT(11) PRIMARY KEY AUTO_INCREMENT COMMENT 'id',
+  blog_id      INT(11) NOT NULL COMMENT '博客id',
+  comment_date DATETIME            DEFAULT NULL COMMENT '评论时间',
+  content      VARCHAR(500)        DEFAULT NULL COMMENT '评论内容',
+  FOREIGN KEY (blog_id) REFERENCES blog (id)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8 COMMENT '评论';
+
+
